616401658052c0ecfe72606e2212243fcf4e11f0
Tuan Anh <tuan73176@gmail.com>
2024-04-04 13:27:16 +0700

    vin8953: Checkout to the latest extract-utils (yet.)
    * From LineageOS/android_tools_extract-utils@41cd5a0.
    
    Change-Id: Iee05eb3bcbdb30c889166725ea435ed7c014f0cf

--- extract-files.sh
+++ extract-files.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 #
-# Copyright (C) 2018-2021 The LineageOS Project
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 set -e
 
-# Required!
-export DEVICE=vin8953
-export VENDOR=vsmart
-
-export DEVICE_BRINGUP_YEAR=2020
+DEVICE=vin8953
+VENDOR=vsmart
 
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
@@ -29,8 +27,8 @@ source "${HELPER}"
 # Default to sanitizing the vendor folder before extraction
 CLEAN_VENDOR=true
 
-SECTION=
 KANG=
+SECTION=
 
 while [ "${#}" -gt 0 ]; do
     case "${1}" in
@@ -57,32 +55,28 @@ fi
 
 function blob_fixup() {
     case "${1}" in
-
     # Fix xml version
-    product/etc/permissions/vendor.qti.hardware.data.connection-V1.0-java.xml | product/etc/permissions/vendor.qti.hardware.data.connection-V1.1-java.xml)
+    product/etc/permissions/vendor.qti.hardware.data.connection-V1.0-java.xml|product/etc/permissions/vendor.qti.hardware.data.connection-V1.1-java.xml)
         sed -i 's/xml version="2.0"/xml version="1.0"/' "${2}"
         ;;
-
     # Remove libhwbinder/libhidltransport dependency
     vendor/lib64/vendor.fpsensor.hardware.fpsensorhidlsvc@2.0.so)
         "${PATCHELF}" --remove-needed "libhwbinder.so" "${2}"
         "${PATCHELF}" --remove-needed "libhidltransport.so" "${2}"
         ;;
-
     vendor/lib64/libfp_ext_svc2.so)
         "${PATCHELF}" --remove-needed "libhwbinder.so" "${2}"
         "${PATCHELF}" --remove-needed "libhidltransport.so" "${2}"
         ;;
-
     vendor/lib/libremosaic_daemon.so)
         "${PATCHELF}" --replace-needed "libbinder.so" "libbinder-v30.so" "${2}"
         ;;
     esac
 }
 
+# Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
-extract "${MY_DIR}/proprietary-files.txt" "${SRC}" \
-        "${KANG}" --section "${SECTION}"
+extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
 
 "${MY_DIR}/setup-makefiles.sh"
