9a3d8aba79dabafbcf307263995b76a3d7474b6b
Bharath <bharath@teamb58.org>
2024-09-07 01:33:14 +0530

    FP3: Update blobs from FP3-REL-6.A.030.0-gms-d57debf7-user
    Change-Id: I97b2ba314370a1e0028150ebee07893b80fb92f6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -274,7 +274,7 @@ BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
 
 
 # Vendor Security Patch Level
-VENDOR_SECURITY_PATCH := "2024-04-05"
+VENDOR_SECURITY_PATCH := "2024-08-05"
 
 
 # Wifi
