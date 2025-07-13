128adae238fccbcaeb13abe570317923437169d1
nhglong <hoanglong.lumi404@gmail.com>
2025-07-13 13:35:18 +0700

    casuarina: Use fstab.qcom also for recovery
    Change-Id: I6a7a35d85cab81d75ed576ede012582e27f569b5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -203,7 +203,7 @@ TARGET_USES_INTERACTION_BOOST := true
 TARGET_SYSTEM_PROP := $(DEVICE_PATH)/system.prop
 
 # Recovery
-TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/recovery.fstab
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
 # RIL
 ENABLE_VENDOR_RIL_SERVICE := true
